43f8bee4de3d2c3c8263ba22422498ee01ce43f8
admin <avikbaidya18@gmail.com>
2026-08-25 01:13:33 +0530

    Complete Day 1 conditionals

--- phase-0/day-1/hello-ai.py
+++ phase-0/day-1/hello-ai.py
@@ -1,5 +1,19 @@
 print("what the fuck is your name?")
 n = input()
 print("How many bananas you have?")
-b = input()
-print("Hello "+n+"! You have "+b+" bananas.")
+b = float(input())
+if(b>10):
+    print("Lot of bananas")
+elif(b>=5 and b<=10): print("Small bunch/ ")
+else:
+    print("Eat the bananas")
+
+print("Cost of each banana: ")
+c = float(input())
+print("Hello "+n+"! You have ", b, " bananas.")
+t=b*c
+print("Total cost of bananas: ", t)
+if(t>100):
+    print("Thats expensive")
+elif(t>50 and t<=100): print("Moderate")
+else:print("Cheap")
